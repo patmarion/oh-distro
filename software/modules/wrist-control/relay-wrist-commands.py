0b8d1c147d7f90ae41c225b7f0b9a9f5bfbd115b
Wolfgang Merkt <w.merkt@gmail.com>
2016-07-25 10:07:38 +0100

    Time synced streaming of lower forearm plans

--- software/modules/wrist-control/relay-wrist-commands.py
+++ software/modules/wrist-control/relay-wrist-commands.py
@@ -3,26 +3,39 @@
 # Filters out the lower arm joints of the COMMITTED_ROBOT_PLAN and 
 # passes them to the Forearm Controller
 
+import time
 import lcm
 import bot_core as lcmbotcore
 import drc as lcmdrc
 
 def onRobotPlan(channel, data):
     msg = lcmdrc.robot_plan_t.decode(data)
-    plan = msg.plan[-1] # last pose
+    planLength = len(msg.plan)
+    totalTime = msg.plan[-1].utime / 1e6
+    print "total time of plan with " + str(planLength) + " waypoints: " + str(totalTime)
 
+    plan = msg.plan[-1]
     leftWristRollIndex = plan.joint_name.index("leftWristRoll")
     leftWristPitchIndex = plan.joint_name.index("leftWristPitch")
     leftForearmYawIndex = plan.joint_name.index("leftForearmYaw")
 
-    leftWristRollRad = plan.joint_position[leftWristRollIndex]
-    leftWristPitchRad = plan.joint_position[leftWristPitchIndex]
-    leftForearmYawRad = plan.joint_position[leftForearmYawIndex]
+    previousUtime = 0
+    startTime = time.time()
+    for plan in msg.plan:
+        planOffset = (plan.utime-previousUtime) / 1e6
+        previousUtime = plan.utime
+        # print "utime: " + str(plan.utime) + " so will sleep for " + str(planOffset)
+        time.sleep(planOffset)
+        leftWristRollRad = plan.joint_position[leftWristRollIndex]
+        leftWristPitchRad = plan.joint_position[leftWristPitchIndex]
+        leftForearmYawRad = plan.joint_position[leftForearmYawIndex]
+        print "Commanding forearm yaw to " + str(leftForearmYawRad)
+        print "Commanding wrist roll to " + str(leftWristRollRad)
+        print "Commanding wrist pitch to " + str(leftWristPitchRad)
+        sendWristCommand(plan.utime, leftForearmYawRad, leftWristRollRad, leftWristPitchRad)
 
-    print "Commanding forearm yaw to " + str(leftForearmYawRad)
-    print "Commanding wrist roll to " + str(leftWristRollRad)
-    print "Commanding wrist pitch to " + str(leftWristPitchRad)
-    sendWristCommand(msg.utime, leftForearmYawRad, leftWristRollRad, leftWristPitchRad)
+    endTime = time.time()
+    print "Done with plan of " + str(totalTime) + "s in " + str(endTime-startTime) + "s"
 
 
 def sendWristCommand(utime, forearmYaw, roll, pitch):
