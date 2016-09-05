ab14130d26f54fe53dd1c53f960049e3473f7dff
Maurice Fallon <mauricefallon@gmail.com>
2016-09-05 23:02:12 +0100

    move maps to maps_server

--- software/perception/opencv-utils/src/print_utils.cpp
+++
@@ -1,7 +0,0 @@
-#include "print_utils.hpp"
-namespace opencv_utils {
-    std::ostream& operator << (std::ostream& os, const cv::Rect& r) {
-        return (os << "[x=" << r.x << ", y=" << r.y << ", w=" << r.width << ", h=" << r.height << "]");
-    }
-    
-}
