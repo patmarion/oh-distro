18792db2e7856ec565d0abaa1f30f50c6023ab46
Wolfgang Merkt <w.merkt@gmail.com>
2016-09-26 18:55:15 +0100

    Update ros2lcm_husky (WIP)

--- catkin_ws/src/oh_translators/src/ros2lcm_husky.cpp
+++ catkin_ws/src/oh_translators/src/ros2lcm_husky.cpp
@@ -1,7 +1,3 @@
-// ### Boost
-#include <boost/thread.hpp>
-#include <boost/shared_ptr.hpp>
-
 // ### ROS
 #include <ros/ros.h>
 #include <ros/console.h>
@@ -22,11 +18,40 @@
 #include <lcmtypes/bot_core.hpp>
 #include <lcm/lcm-cpp.hpp>
 
+struct JointState {
+  double position, velocity, effort;
+
+  JointState() {
+    position = 0.0;
+    velocity = 0.0;
+    effort = 0.0;
+  };
+  JointState(double position_in, double velocity_in, double effort_in) {
+    position = position_in;
+    velocity = velocity_in;
+    effort = effort_in;
+  }
+};
+
+struct Pose {
+  double position[3];
+  double orientation[4];  // w, x, y, z
+
+  Pose() {
+    for (int i = 0; i < 3; i++)
+      position[i] = 0.0;
+
+    orientation[0] = 1.0;
+    orientation[1] = 0.0;
+    orientation[2] = 0.0;
+    orientation[3] = 0.0;
+  }
+};
 
 class App
 {
 public:
-  explicit App(ros::NodeHandle node_);
+  explicit App(ros::NodeHandle node, std::string husky_type);
   ~App();
 
 private:
@@ -47,37 +72,125 @@ private:
 
   ros::Subscriber headJointStatesSub_;
   void publishMultisenseState(int64_t utime, float position, float velocity);
-  void headJointStatesCallback(const sensor_msgs::JointStateConstPtr& msg);
+  void jointStatesCallback(const sensor_msgs::JointStateConstPtr& msg);
 
+  // Switch between Multisense and Dual Arm Huskies
+  std::string husky_type_;
+  bool publish_est_robot_state_from_ekf_;
+
+  // Store joint states internally and compose EST_ROBOT_STATE
+  void UpdateInternalStateFromJointStatesMsg(const sensor_msgs::JointStateConstPtr& msg);
+  void PublishEstRobotStateFromInternalState(int64_t utime);
+
+  // Joint names
+  std::vector<std::string> floating_base_ = {
+      "base_x", "base_y", "base_z", "base_roll", "base_pitch", "base_yaw"};
+  std::vector<std::string> dual_arm_husky_joints_ = {
+      "front_left_wheel", "front_right_wheel", "rear_left_wheel",
+      "rear_right_wheel", "l_ur5_arm_shoulder_pan_joint",
+      "l_ur5_arm_shoulder_lift_joint", "l_ur5_arm_elbow_joint",
+      "l_ur5_arm_wrist_1_joint", "l_ur5_arm_wrist_2_joint",
+      "l_ur5_arm_wrist_3_joint", "r_ur5_arm_shoulder_pan_joint",
+      "r_ur5_arm_shoulder_lift_joint", "r_ur5_arm_elbow_joint",
+      "r_ur5_arm_wrist_1_joint", "r_ur5_arm_wrist_2_joint",
+      "r_ur5_arm_wrist_3_joint", "l_palm_finger_1_joint", "l_finger_1_joint_1",
+      "l_finger_1_joint_2", "l_finger_1_joint_3",
+      "l_finger_1_joint_proximal_actuating_hinge",
+      "l_finger_1_joint_paraproximal_actuating_hinge",
+      "l_finger_1_joint_proximal_actuating_bar",
+      "l_finger_1_joint_paraproximal_bar",
+      "l_finger_1_joint_median_actuating_hinge",
+      "l_finger_1_joint_median_actuating_hinge_median_bar",
+      "l_finger_1_joint_paramedian_hinge",
+      "l_finger_1_joint_paramedian_hinge_median_bar_underactuated",
+      "l_finger_1_joint_paradistal_hinge", "l_palm_finger_2_joint",
+      "l_finger_2_joint_1", "l_finger_2_joint_2", "l_finger_2_joint_3",
+      "l_finger_2_joint_proximal_actuating_hinge",
+      "l_finger_2_joint_paraproximal_actuating_hinge",
+      "l_finger_2_joint_proximal_actuating_bar",
+      "l_finger_2_joint_paraproximal_bar",
+      "l_finger_2_joint_median_actuating_hinge",
+      "l_finger_2_joint_median_actuating_hinge_median_bar",
+      "l_finger_2_joint_paramedian_hinge",
+      "l_finger_2_joint_paramedian_hinge_median_bar_underactuated",
+      "l_finger_2_joint_paradistal_hinge", "l_palm_finger_middle_joint",
+      "l_finger_middle_joint_1", "l_finger_middle_joint_2",
+      "l_finger_middle_joint_3",
+      "l_finger_middle_joint_proximal_actuating_hinge",
+      "l_finger_middle_joint_paraproximal_actuating_hinge",
+      "l_finger_middle_joint_proximal_actuating_bar",
+      "l_finger_middle_joint_paraproximal_bar",
+      "l_finger_middle_joint_median_actuating_hinge",
+      "l_finger_middle_joint_median_actuating_hinge_median_bar",
+      "l_finger_middle_joint_paramedian_hinge",
+      "l_finger_middle_joint_paramedian_hinge_median_bar_underactuated",
+      "l_finger_middle_joint_paradistal_hinge", "r_palm_finger_1_joint",
+      "r_finger_1_joint_1", "r_finger_1_joint_2", "r_finger_1_joint_3",
+      "r_finger_1_joint_proximal_actuating_hinge",
+      "r_finger_1_joint_paraproximal_actuating_hinge",
+      "r_finger_1_joint_proximal_actuating_bar",
+      "r_finger_1_joint_paraproximal_bar",
+      "r_finger_1_joint_median_actuating_hinge",
+      "r_finger_1_joint_median_actuating_hinge_median_bar",
+      "r_finger_1_joint_paramedian_hinge",
+      "r_finger_1_joint_paramedian_hinge_median_bar_underactuated",
+      "r_finger_1_joint_paradistal_hinge", "r_palm_finger_2_joint",
+      "r_finger_2_joint_1", "r_finger_2_joint_2", "r_finger_2_joint_3",
+      "r_finger_2_joint_proximal_actuating_hinge",
+      "r_finger_2_joint_paraproximal_actuating_hinge",
+      "r_finger_2_joint_proximal_actuating_bar",
+      "r_finger_2_joint_paraproximal_bar",
+      "r_finger_2_joint_median_actuating_hinge",
+      "r_finger_2_joint_median_actuating_hinge_median_bar",
+      "r_finger_2_joint_paramedian_hinge",
+      "r_finger_2_joint_paramedian_hinge_median_bar_underactuated",
+      "r_finger_2_joint_paradistal_hinge", "r_palm_finger_middle_joint",
+      "r_finger_middle_joint_1", "r_finger_middle_joint_2",
+      "r_finger_middle_joint_3",
+      "r_finger_middle_joint_proximal_actuating_hinge",
+      "r_finger_middle_joint_paraproximal_actuating_hinge",
+      "r_finger_middle_joint_proximal_actuating_bar",
+      "r_finger_middle_joint_paraproximal_bar",
+      "r_finger_middle_joint_median_actuating_hinge",
+      "r_finger_middle_joint_median_actuating_hinge_median_bar",
+      "r_finger_middle_joint_paramedian_hinge",
+      "r_finger_middle_joint_paramedian_hinge_median_bar_underactuated",
+      "r_finger_middle_joint_paradistal_hinge"};
+  std::map<std::string, JointState> joint_states_;
+  Pose pose_;
 };
 
-App::App(ros::NodeHandle node_)
-{
-  ROS_INFO("Initializing Sick Translator");
-  if (!lcmPublish_.good())
-  {
-    std::cerr << "ERROR: lcm is not good()" << std::endl;
+App::App(ros::NodeHandle node, std::string husky_type)
+    : node_(node), husky_type_(husky_type), publish_est_robot_state_from_ekf_(false) {
+  if (!lcmPublish_.good()) std::cerr << "ERROR: lcm is not good()" << std::endl;
+
+  if (husky_type != "multisense" && husky_type != "dual_arm")
+    throw std::runtime_error(
+        "Unsupported mode: Only multisense and dual_arm modes supported.");
+
+  // Multisense Husky
+  if (husky_type_ == "multisense") {
+    publish_est_robot_state_from_ekf_ = true;
+  } else if (husky_type_ == "dual_arm") {
+    // for (auto& joint : dual_arm_husky_joints_)
+    //   joint_states_.insert(std::make_pair(joint, JointState()));
   }
 
-  sick_lidar_sub_ = node_.subscribe(std::string("/sick_scan"), 100, &App::sick_lidar_cb,
-                                      this);
-  spinning_lidar_sub_ = node_.subscribe(std::string("/lidar_scan"), 100, &App::spinning_lidar_cb,
-                                      this);
-  ekf_odom_sub_ = node_.subscribe(std::string("/robot_pose_ekf/odom_combined"), 100, &App::ekf_odom_cb,
-                                    this);
+  sick_lidar_sub_ = node_.subscribe(std::string("/sick_scan"), 100,
+                                    &App::sick_lidar_cb, this);
+  spinning_lidar_sub_ = node_.subscribe(std::string("/lidar_scan"), 100,
+                                        &App::spinning_lidar_cb, this);
+  ekf_odom_sub_ = node_.subscribe(std::string("/robot_pose_ekf/odom_combined"),
+                                  100, &App::ekf_odom_cb, this);
 
   imuSensorSub_ = node_.subscribe(std::string("/imu/data"), 100,
-                                    &App::imuSensorCallback, this);
+                                  &App::imuSensorCallback, this);
 
-  headJointStatesSub_ = node_.subscribe(std::string("/joint_states"), 100, &App::headJointStatesCallback,
-                                            this);
+  headJointStatesSub_ = node_.subscribe(std::string("/joint_states"), 100,
+                                        &App::jointStatesCallback, this);
 }
 
-App::~App()
-{
-}
-
-
+App::~App() {}
 
 void App::imuSensorCallback(const sensor_msgs::ImuConstPtr& msg)
 {
@@ -100,7 +213,7 @@ void App::imuSensorCallback(const sensor_msgs::ImuConstPtr& msg)
   imu.pressure = 0;
   imu.rel_alt = 0;
 
-  lcmPublish_.publish( "IMU_MICROSTRAIN", &imu);
+  lcmPublish_.publish("IMU_MICROSTRAIN", &imu);
 }
 
 
@@ -129,7 +242,13 @@ void App::ekf_odom_cb(const geometry_msgs::PoseWithCovarianceStampedConstPtr& ms
   lcm_pose_msg.orientation[3] = msg->pose.pose.orientation.z;
   lcmPublish_.publish("POSE_BODY", &lcm_pose_msg);
 
-  if (1==1){ // publish EST_ROBOT_STATE (used to draw the robot)
+  // Update internal pose state
+  for (int i = 0; i < 3; i++)
+    pose_.position[i] = lcm_pose_msg.pos[i];
+  for (int i = 0; i < 4; i++)
+    pose_.orientation[i] = lcm_pose_msg.orientation[i];
+
+  if (publish_est_robot_state_from_ekf_) { // publish EST_ROBOT_STATE (used to draw the robot)
     bot_core::robot_state_t est_robot_state_;
     est_robot_state_.utime = (int64_t)msg->header.stamp.toNSec() / 1000;  // from nsec to usec
     est_robot_state_.num_joints = 4;
@@ -174,7 +293,6 @@ void App::ekf_odom_cb(const geometry_msgs::PoseWithCovarianceStampedConstPtr& ms
 
     lcmPublish_.publish("EST_ROBOT_STATE", &est_robot_state_);
   }
-
 }
 
 
@@ -202,28 +320,106 @@ void App::publishLidar(const sensor_msgs::LaserScanConstPtr& msg, std::string ch
   lcmPublish_.publish(channel.c_str(), &scan_out);
 }
 
+void App::jointStatesCallback(const sensor_msgs::JointStateConstPtr& msg) {
+  int64_t utime =
+      static_cast<int64_t>(floor(msg->header.stamp.toNSec() / 1000));
 
+  // Multisense Husky Mode: Publish Multisense Hokuyo state
+  if (husky_type_ == "multisense" && msg->name.size() == 1 &&
+      msg->name[0] == "hokuyo_joint")
+    return publishMultisenseState(utime, msg->position[0], msg->velocity[0]);
 
-void App::headJointStatesCallback(const sensor_msgs::JointStateConstPtr& msg)
-{
-  if (msg->name.size() > 1)
-  {
-    // ROS_ERROR("Error: Unrecognised multisense joint: %s",msg->name[0].c_str());
-    return;
+  // Store incoming joints in internal state
+  UpdateInternalStateFromJointStatesMsg(msg);
+
+  // TODO: dual arm husky: publish left and right arms separately
+
+  // We got the wheels, so publish joint state
+  // NB: We use the wheels, even though at lower frequency, to trigger the
+  // publishing of the generated message as these will always be available.
+  // Hands or arms, which might be powered down, are not generally available at
+  // all times.
+  // TODO: create a dedicated state sync
+  if (msg->name.size() == 4) PublishEstRobotStateFromInternalState(utime);
+}
+
+void App::UpdateInternalStateFromJointStatesMsg(
+    const sensor_msgs::JointStateConstPtr& msg) {
+  size_t num_joints = msg->name.size();
+
+  for (size_t i = 0; i < num_joints; i++) {
+    // NB: Some joint states do not contain velocity or effort parts, this
+    // catches exceptions
+    double position = (msg->position.size() > 0) ? msg->position[i] : 0.0;
+    double velocity = (msg->velocity.size() > 0) ? msg->velocity[i] : 0.0;
+    double effort = (msg->effort.size() > 0) ? msg->effort[i] : 0.0;
+
+    // Find in map, if not create
+    if (joint_states_.find(msg->name[i]) == joint_states_.end()) {
+      ROS_WARN_STREAM("Joint " << msg->name[i]
+                               << " not found in internal state - this should "
+                                  "not happen due to pre-allocation -- check "
+                                  "correct pre-allocation");
+      JointState new_joint_state(position, velocity, effort);
+      joint_states_.insert(std::make_pair(msg->name[i], new_joint_state));
+    } else {
+      JointState& joint_state = joint_states_[msg->name[i]];
+      joint_state.position = position;
+      joint_state.velocity = velocity;
+      joint_state.effort = effort;
+    }
+  }
+}
+
+void App::PublishEstRobotStateFromInternalState(int64_t utime) {
+  bot_core::robot_state_t msg;
+  msg.utime = utime;
+  msg.num_joints = joint_states_.size();
+  msg.pose.translation.x = pose_.position[0];
+  msg.pose.translation.y = pose_.position[1];
+  msg.pose.translation.z = pose_.position[2];
+  msg.pose.rotation.w = pose_.orientation[0];
+  msg.pose.rotation.x = pose_.orientation[1];
+  msg.pose.rotation.y = pose_.orientation[2];
+  msg.pose.rotation.z = pose_.orientation[3];
+  msg.twist.linear_velocity.x = 0.0;
+  msg.twist.linear_velocity.y = 0.0;
+  msg.twist.linear_velocity.z = 0.0;
+  msg.twist.angular_velocity.x = 0.0;
+  msg.twist.angular_velocity.y = 0.0;
+  msg.twist.angular_velocity.z = 0.0;
+
+  // Initialise F/T sensors in msg
+  msg.force_torque.l_foot_force_z = 0.0;
+  msg.force_torque.l_foot_torque_x = 0.0;
+  msg.force_torque.l_foot_torque_y = 0.0;
+  msg.force_torque.r_foot_force_z = 0.0;
+  msg.force_torque.r_foot_torque_x = 0.0;
+  msg.force_torque.r_foot_torque_y = 0.0;
+
+  for (unsigned int i = 0; i < 3; i++) {
+      msg.force_torque.l_hand_force[i] = 0.0;
+      msg.force_torque.l_hand_torque[i] = 0.0;
+      msg.force_torque.r_hand_force[i] = 0.0;
+      msg.force_torque.r_hand_torque[i] = 0.0;
   }
 
-  int64_t utime = (int64_t)floor(msg->header.stamp.toNSec() / 1000);
-  publishMultisenseState(utime, msg->position[0], msg->velocity[0]);
+  msg.joint_name.assign(msg.num_joints, "");
+  msg.joint_position.assign(msg.num_joints, (const float &) 0.);
+  msg.joint_velocity.assign(msg.num_joints, (const float &) 0.);
+  msg.joint_effort.assign(msg.num_joints, (const float &) 0.);
+  
+  // Iterate over joint_states_ map
+  int joint_number = 0;
+  for (auto& joint : joint_states_) {
+    msg.joint_name[joint_number] = joint.first;
+    msg.joint_position[joint_number] = joint.second.position;
+    msg.joint_velocity[joint_number] = joint.second.velocity;
+    msg.joint_effort[joint_number] = joint.second.effort;
+    joint_number++;
+  }
 
-  /*
-   std::string jname = "hokuyo_joint";
-   int i = std::distance( msg->name.begin(), std::find( msg->name.begin(), msg->name.end(), jname ) );
-   if( i == msg->name.size() ){
-   // std::cout << "not found: " << jname << "\n";
-   }else{
-   publishMultisenseState(utime, msg->position[i], msg->velocity[i]);
-   }
-   */
+  lcmPublish_.publish("EST_ROBOT_STATE", &msg);
 }
 
 void App::publishMultisenseState(int64_t utime, float position, float velocity)
@@ -250,14 +446,16 @@ void App::publishMultisenseState(int64_t utime, float position, float velocity)
   lcmPublish_.publish("PRE_SPINDLE_TO_POST_SPINDLE", &preToPostFrame);
 }
 
-
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
   ros::init(argc, argv, "ros2lcm_husky");
   ros::NodeHandle nh;
-  new App(nh);
-  ROS_INFO_STREAM("ros2lcm_husky translator ready");
-  ROS_ERROR_STREAM("ros2lcm_husky translator ready");
+
+  // std::string husky_type = "multisense";
+  std::string husky_type = "dual_arm";
+
+  new App(nh, husky_type);
+  ROS_INFO_STREAM("ros2lcm_husky translator ready: " << husky_type);
+  ROS_ERROR_STREAM("ros2lcm_husky translator ready: " << husky_type);
   ros::spin();
   return 0;
 }
