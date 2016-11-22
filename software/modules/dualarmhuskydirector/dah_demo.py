380bc5a0568fdce2a1b061b124d14af8eb6f186a
stoutheo06 <stoutheo@gmail.com>
2016-11-22 19:30:19 +0000

    Dual Arm Husky	update
    	i)   dual arm huksy demo (pick and drop)
    	ii)  motion planning high level class
    	iii) botFrame for the rubbish bin

--- software/modules/dualarmhuskydirector/dah_demo.py
+++ software/modules/dualarmhuskydirector/dah_demo.py
@@ -10,31 +10,69 @@ class demo(object):
         # init segmentation class
         self.segsc = segmentScene.segScene(view)
 
-        
-    # def setupBox(self,getBotFrame,affordancePanel,playbackRobotModel,vis):
+        # tolerance limits
+        self.flexangTol = 60
+        self.stricktangTol = 5
 
-    #     frameSync, boxFrame=botFrameSync.createBoxAffordanceAndSyncToBotFrame(getBotFrame,affordancePanel,playbackRobotModel,vis)
-        
-    #     return frameSync, boxFrame
+        # configuration list
+        self.qs = []
 
-    # def createGraspFrameTo
+    def setupBox(self,getBotFrame,affordancePanel,vis):
+        """
+        """
+        # Create the box affordance and sync it to bot frame RUBBISH_BIN
+        # Assumes RUBBISH_BIN exists.
+        frameSync, boxFrame = botFrameSync.createBoxAndSync(getBotFrame,affordancePanel,vis)
+         
+        return frameSync, boxFrame
+
+    def createDropFrame(self, boxFrame, vis):
+        """
+        """
+        # Check 
+        
+
+        dropFrameTrans = botFrameSync.createDropFrameAboveBox(boxFrame,vis) # uses default name
+
+        return dropFrameTrans
 
     def segmentTable(self, oDPC=None):
 
         # Generate a frame called 'object X newframe' X = 0,1 
         self.segsc.demoSegment(oDPC)
 
-    def moveArm(self, arm, frameName, objInHand=False):
+    def moveArm(self, arm, frameName, objInHand=False, flex = False):
         
+        # Syncs current state within class with actual robot state
+        self.motionplan.synchRobot()
+
+        # Selects between flexible tolerance in the orientation
+        if flex:
+            aT = self.flexangTol
+        else:
+            aT = self.stricktangTol
+
+
+        # Selects between grasp or pre-grasp frame
         if objInHand:
             self.motionplan.setGraspFrame()
+        else:
+            self.motionplan.setPreGraspFrame()
 
+        # Select arm and request motion plan
         if arm == 'left':
-            self.motionplan.reachLeft(frameName)
+            self.motionplan.reachLeft(frameName, aT)
         elif arm == 'right':
-            self.motionplan.reachRight(frameName)
+            self.motionplan.reachRight(frameName, aT)
         else:
             print 'Error: not recognised arm:', arm
 
+
+    def store_q(self):
+
+        # append in a list the current configuration
+        self.qs.append(self.motionplan.pU.getPlanningStartPose())
+
     
+
     
