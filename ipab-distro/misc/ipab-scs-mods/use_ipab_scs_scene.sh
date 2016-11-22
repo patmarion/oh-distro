969e6ba5c960a8c1a9d417c8028cf82d7e634da9
Wolfgang Merkt <w.merkt@gmail.com>
2016-11-22 10:47:07 +0000

    Nuke ipab-distro: unused and broken system tests and SCS modifications

--- ipab-distro/misc/ipab-scs-mods/use_ipab_scs_scene.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Copying EdinburghEnvironment.java into location"
-cp EdinburghEnvironment.java  ~/workspace/DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/environment/DRCDemo01NavigationEnvironment.java
-
-# startingLocation = DRCObstacleCourseStartingLocation.valueOf("STEPPING_STONES");
-# cp ContinuousWalkingEnvironment.java  ~/workspace/DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/environment/DRCDemo01NavigationEnvironment.java
