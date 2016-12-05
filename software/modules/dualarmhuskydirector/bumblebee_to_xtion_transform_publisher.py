2cbd88fd943385ec7168a4409796ac41f298b254
Wolfgang Merkt <w.merkt@gmail.com>
2016-12-05 11:33:50 +0000

    update submodules

--- software/modules/dualarmhuskydirector/bumblebee_to_xtion_transform_publisher.py
+++ software/modules/dualarmhuskydirector/bumblebee_to_xtion_transform_publisher.py
@@ -8,7 +8,8 @@ from bot_core.rigid_transform_t import rigid_transform_t
 lc = lcm.LCM()
 msg = rigid_transform_t();
 msg.utime = 0;
-msg.trans = [-0.048, -0.03, -0.01]
+msg.trans = [-0.01, -1.225, -.225]
+#[float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3])]#[-0.048, -0.03, -0.01]
 msg.quat = botpy.euler_to_quat([float(sys.argv[1])*np.pi/180.0, float(sys.argv[2])*np.pi/180.0, float(sys.argv[3])*np.pi/180.0])
 
 lc.publish("BB_TO_OPENNI_FRAME_LEFT", msg.encode())
