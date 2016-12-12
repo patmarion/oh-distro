41c304da70bf2729c5734f2b5c90b7f0516a2c0e
Wolfgang Merkt <w.merkt@gmail.com>
2016-12-12 15:32:53 +0000

    script updated

--- software/modules/dualarmhuskydirector/bumblebee_to_xtion_transform_publisher.py
+++ software/modules/dualarmhuskydirector/bumblebee_to_xtion_transform_publisher.py
@@ -8,8 +8,18 @@ from bot_core.rigid_transform_t import rigid_transform_t
 lc = lcm.LCM()
 msg = rigid_transform_t();
 msg.utime = 0;
-msg.trans = [-0.01, -1.225, -.225]
-#[float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3])]#[-0.048, -0.03, -0.01]
-msg.quat = botpy.euler_to_quat([float(sys.argv[1])*np.pi/180.0, float(sys.argv[2])*np.pi/180.0, float(sys.argv[3])*np.pi/180.0])
+# msg.trans = [-0.01, -1.225, -.225]
+# msg.quat = botpy.euler_to_quat([float(sys.argv[1])*np.pi/180.0, float(sys.argv[2])*np.pi/180.0, float(sys.argv[3])*np.pi/180.0])
+
+x = float(sys.argv[1]) #-0.01
+y = float(sys.argv[2]) #-1.225
+z = float(sys.argv[3]) #-.225
+r = float(sys.argv[4]) #-35 #float(sys.argv[1])
+p = float(sys.argv[5]) #0 #float(sys.argv[2])
+yaw = float(sys.argv[6]) #0#float(sys.argv[3])
+
+msg.trans = [x, y, z]
+msg.quat = botpy.euler_to_quat([r*np.pi/180.0, p*np.pi/180.0, yaw*np.pi/180.0])
+
 
 lc.publish("BB_TO_OPENNI_FRAME_LEFT", msg.encode())
