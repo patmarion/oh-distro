cbcc4a877917a21fa74a6479ccd87091454934b5
Wolfgang Merkt <w.merkt@gmail.com>
2017-02-03 11:35:44 +0000

    Update iris-distro to master to fix #2609

--- software/externals/cmake/externals.cmake
+++ software/externals/cmake/externals.cmake
@@ -155,7 +155,7 @@ set(swigmake_revision 7e4fcbeb46c5fc7b1efb651b4365ba8b777ef184)
 set(swigmake_depends )
 
 set(iris_url https://github.com/rdeits/iris-distro.git)
-set(iris_revision f6cdf5a4f7896054922fd93276754f83a5497eff)
+set(iris_revision a895e8af0850242c09641c31ea8b12bacb34fd2b)
 set(iris_depends Eigen_pod)
 set(iris_external_args
   CMAKE_CACHE_ARGS
